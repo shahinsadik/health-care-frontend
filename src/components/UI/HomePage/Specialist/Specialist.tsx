6784fc6c19921a8b51aa7232eee348a6118efa93
shahinsadik <shahinsadik@gmail.com>
2025-06-05 09:20:28 +0600

    cjhangr

---
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -0,0 +1,21 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+
+const Specialist = () => {
+  return (
+    <Container sx={{ margin: "40px 0px", textAlign: "center" }}>
+      <Box>
+        <Box textAlign="start">
+          <Typography variant="h4" fontWeight={600}>
+            Explore Treatment Across Specialties
+          </Typography>
+          <Typography color="primary.secondary" component="p" fontWeight={300} fontSize={18}>
+            Experience Doctor Across All Specialties
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Specialist;
